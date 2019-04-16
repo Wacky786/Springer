9d259b049ff19d4cad430b5f3a0620c8c1730a6e
Wacky786 <waqas786uk@gmail.com>
2019-04-16 20:49:52 +0100

    Added second test: "I search with non-alpha characters"

--- src/specs/search.js
+++ src/specs/search.js
@@ -10,5 +10,17 @@ module.exports = {
             .waitForElementVisible("[id=results]", 5000)
             .assert.urlContains("/search?query=")
             .end();
+    },
+
+    "I search with non-alpha characters" : browser => {
+        browser
+            .url("https://link.springer.com/")
+            .waitForElementVisible("[id=query]", 5000)
+            .setValue("[id=query]", "!@£$%^&*()_+}{|:?><~-=][\';/.,`\"")
+            .waitForElementVisible("[id=search]", 5000)
+            .click("[id=search]")
+            .waitForElementVisible("[id=no-results-message]", 5000)
+            .assert.urlContains("/search?query=")
+            .end();
     }
 }
