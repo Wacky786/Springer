64d4f8f5136bf281f1a1d2224d64f0be16ef8843
Wacky786 <waqas786uk@gmail.com>
2019-04-16 21:50:13 +0100

    Added search_page.js Page Object Updated search specs to use search_page PO

--- src/specs/search.js
+++ src/specs/search.js
@@ -2,42 +2,48 @@
 module.exports = {
     "I search for a journal" : browser => {
         var homepage = browser.page.homepage();
+        var search_page = browser.page.search_page();
 
         homepage
             .navigate()
             .waitForElementVisible("@searchInput", 5000)
             .setValue("@searchInput", "big data testing")
             .waitForElementVisible("@searchButton", 5000)
-            .click("@searchButton")
-            .waitForElementVisible("[id=results]", 5000)
+            .click("@searchButton");
+        search_page
+            .waitForElementVisible("@searchResults", 5000)
             .assert.urlContains("/search?query=")
             .end();
     },
 
     "I search with non-alpha characters" : browser => {
         var homepage = browser.page.homepage();
+        var search_page = browser.page.search_page();
 
         homepage
             .navigate()
             .waitForElementVisible("@searchInput", 5000)
             .setValue("@searchInput", "!@£$%^&*()_+}{|:?><~-=][\';/.,`\"")
             .waitForElementVisible("@searchButton", 5000)
-            .click("@searchButton")
-            .waitForElementVisible("[id=no-results-message]", 5000)
+            .click("@searchButton");
+        search_page
+            .waitForElementVisible("@searchResultsEmpty", 5000)
             .assert.urlContains("/search?query=")
             .end();
     },
 
     "I search with a long 1000 character query" : browser => {
         var homepage = browser.page.homepage();
+        var search_page = browser.page.search_page();
 
         homepage
             .navigate()
             .waitForElementVisible("@searchInput", 5000)
-            .setValue("@searchInput", "dsfnkdfndsifnsoidfjdsjfoiasdfnoisdanfoisdafnsodfnoidsafnsodfnosdinfosdfnofioidfjsdaiofjsdiofosdifhisdahfisdufishfiuasfoasiJDOSAdjoasjoaDJIOajdoiasdjoasdjdsfnkdfndsifnsoidfjdsjfoiasdfnoisdanfoisdafnsodfnoidsafnsodfnosdinfosdfnofioidfjsdaiofjsdiofosdifhisdahfisdufishfiuasfoasiJDOSAdjoasjoaDJIOajdoiasdjoasdjdsfnkdfndsifnsoidfjdsjfoiasdfnoisdanfoisdafnsodfnoidsafnsodfnosdinfosdfnofioidfjsdaiofjsdiofosdifhisdahfisdufishfiuasfoasiJDOSAdjoasjoaDJIOajdoiasdjoasdjdsfnkdfndsifnsoidfjdsjfoiasdfnoisdanfoisdafnsodfnoidsafnsodfnosdinfosdfnofioidfjsdaiofjsdiofosdifhisdahfisdufishfiuasfoasiJDOSAdjoasjoaDJIOajdoiasdjoasdjdsfnkdfndsifnsoidfjdsjfoiasdfnoisdanfoisdafnsodfnoidsafnsodfnosdinfosdfnofioidfjsdaiofjsdiofosdifhisdahfisdufishfiuasfoasiJDOSAdjoasjoaDJIOajdoiasdjoasdjdsfnkdfndsifnsoidfjdsjfoiasdfnoisdanfoisdafnsodfnoidsafnsodfnosdinfosdfnofioidfjsdaiofjsdiofosdifhisdahfisdufishfiuasfoasiJDOSAdjoasjoaDJIOajdoiasdjoasdjdsfnkdfndsifnsoidfjdsjfoiasdfnoisdanfoisdafnsodfnoidsafnsodfnosdinfosdfnofioidfjsdaiofjsdiofosdifhisdahfisdufishfiuasfoasiJDOSAdjoasjo")
+            .setValue("[id=query]", "dsfnkdfndsifnsoidfjdsjfoiasdfnoisdanfoisdafnsodfnoidsafnsodfnosdinfosdfnofioidfjsdaiofjsdiofosdifhisdahfisdufishfiuasfoasiJDOSAdjoasjoaDJIOajdoiasdjoasdjdsfnkdfndsifnsoidfjdsjfoiasdfnoisdanfoisdafnsodfnoidsafnsodfnosdinfosdfnofioidfjsdaiofjsdiofosdifhisdahfisdufishfiuasfoasiJDOSAdjoasjoaDJIOajdoiasdjoasdjdsfnkdfndsifnsoidfjdsjfoiasdfnoisdanfoisdafnsodfnoidsafnsodfnosdinfosdfnofioidfjsdaiofjsdiofosdifhisdahfisdufishfiuasfoasiJDOSAdjoasjoaDJIOajdoiasdjoasdjdsfnkdfndsifnsoidfjdsjfoiasdfnoisdanfoisdafnsodfnoidsafnsodfnosdinfosdfnofioidfjsdaiofjsdiofosdifhisdahfisdufishfiuasfoasiJDOSAdjoasjoaDJIOajdoiasdjoasdjdsfnkdfndsifnsoidfjdsjfoiasdfnoisdanfoisdafnsodfnoidsafnsodfnosdinfosdfnofioidfjsdaiofjsdiofosdifhisdahfisdufishfiuasfoasiJDOSAdjoasjoaDJIOajdoiasdjoasdjdsfnkdfndsifnsoidfjdsjfoiasdfnoisdanfoisdafnsodfnoidsafnsodfnosdinfosdfnofioidfjsdaiofjsdiofosdifhisdahfisdufishfiuasfoasiJDOSAdjoasjoaDJIOajdoiasdjoasdjdsfnkdfndsifnsoidfjdsjfoiasdfnoisdanfoisdafnsodfnoidsafnsodfnosdinfosdfnofioidfjsdaiofjsdiofosdifhisdahfisdufishfiuasfoasiJDOSAdjoasjo")
             .waitForElementVisible("@searchButton", 5000)
-            .click("@searchButton")
-            .waitForElementVisible("[id=no-results-message]", 5000)
+            .click("@searchButton");
+        search_page
+            .waitForElementVisible("@searchResultsEmpty", 5000)
             .assert.urlContains("/search?query=")
             .end();
     },
@@ -50,9 +56,10 @@ module.exports = {
             .waitForElementVisible("@searchInput", 5000)
             .waitForElementVisible("@searchButton", 5000)
             .click("@searchButton")
-            .assert.urlEquals("https://link.springer.com/")
+            .assert.urlEquals(homepage.url)
             .end();
     }
+
 
     
 }
